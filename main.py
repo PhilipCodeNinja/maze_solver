1ae25e99b1276b2a633c09494fd3e0063223642d
PhilipCodeNinja <philip@famreiber.de>
2025-03-14 16:04:11 +0000

    Cells can be drawn on the canvas.

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from window import *
+from graphics import *
+from cell import*
 
-win = Window(800, 600)
-win.wait_for_close()
+def main():
+    win = Window(800, 600)
+    c1 = Cell(win)
+    c1.draw(100,100,200,200)
+
+    win.wait_for_close()
+
+if __name__ == "__main__":
+    main()
